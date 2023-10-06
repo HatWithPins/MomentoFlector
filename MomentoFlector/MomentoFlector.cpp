dcf81457c126ddc0d8b618c8ecb6973691788211
HatWithPins <daniel.carril4@gmail.com>
2023-10-06 13:02:43 +0200

    Command line argument parser

--- MomentoFlector/MomentoFlector.cpp
+++ MomentoFlector/MomentoFlector.cpp
@@ -2,8 +2,50 @@
 
 using namespace std;
 
-int main()
+int main(int argc, char** argv)
 {
-	cout << "Hello CMake." << endl;
+	string argument;
+	size_t pos;
+	size_t check;
+	double boundLeft;
+	double boundRight;
+	int N;
+	bool multithread;
+	int expectedArguments = 5;
+	vector<string> expectedArgumentsList = {"bound_left=", "bound_right=", "N=", "multithread="};
+	vector<string> receivedArguments;
+
+	if (argc != expectedArguments)
+	{
+		cout << "Error, expected " << expectedArguments - 1 << ", but received " << argc - 1 << endl;
+		return -1;
+	}
+
+
+	try 
+	{
+		for (int i = 1; i < argc; i++)
+		{
+			argument = argv[i];
+			check = argument.find(expectedArgumentsList[i - 1]);
+			if (check < 0)
+			{
+				throw(argument, expectedArgumentsList[i - 1]);
+			}
+			pos = argument.find("=");
+			receivedArguments.push_back(argument.substr(pos + 1));
+		}
+	}
+	catch (vector<string> error)
+	{
+		cout << "Error, expected " << error[1] << ", but received " << error[0] << endl;
+		return -1;
+	}
+
+	boundLeft = stod(receivedArguments[0]);
+	boundRight = stod(receivedArguments[1]);
+	N = stoi(receivedArguments[2]);
+	multithread = stoi(receivedArguments[3]);
+	
 	return 0;
 }
--- MomentoFlector/MomentoFlector.h
+++ MomentoFlector/MomentoFlector.h
@@ -1,8 +1,6 @@
-﻿// MomentoFlector.h : Include file for standard system include files,
-// or project specific include files.
-
-#pragma once
+﻿#pragma once
 
 #include <iostream>
-
-// TODO: Reference additional headers your program requires here.
+#include <thread>
+#include <string>
+#include <vector>
